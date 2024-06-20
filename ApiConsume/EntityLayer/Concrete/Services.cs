b2409113bf29bd751ad41c16c0df94a079058fea
Oguzhan Kotan <81297977+OguzhanKtn@users.noreply.github.com>
2024-06-20 13:52:34 +0300

    ProjectStarted

---
+++ ApiConsume/EntityLayer/Concrete/Services.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Services
+    {
+        public int ServiceID { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
