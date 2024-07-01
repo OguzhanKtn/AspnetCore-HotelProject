c448148722afbc6e9a77127cdedc974b87761d9e
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2024-07-01 22:23:45 +0300

    -

--- ApiConsume/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ ApiConsume/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2e8132cbf31d100bdd2816c31e8eeb37b868f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/WebUI/Controllers/AdminRoomController.cs
+++ Frontend/WebUI/Controllers/AdminRoomController.cs
@@ -4,8 +4,17 @@ namespace WebUI.Controllers
 {
     public class AdminRoomController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminRoomController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
+            var client = _httpClientFactory.CreateClient();
+
             return View();
         }
     }
